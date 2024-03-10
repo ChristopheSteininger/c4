c423d060366a55f5ed3db101cb85e7278659f719
Chris <christophe.steininger@gmail.com>
2024-03-10 15:33:32 +0000

    Update compiler options for MSVC builds.

--- src/solver/os.h
+++ src/solver/os.h
@@ -4,6 +4,8 @@
 #include <cstdint>
 #include <thread>
 
+// This file defines any OS specific utilities used by the search.
+
 void *allocate_huge_pages(size_t count, size_t size);
 
 void free_huge_pages(void *memory);
