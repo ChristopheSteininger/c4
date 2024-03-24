347f4102b05d7cc03f44e04ff39b1d7786434b4f
Christophe Steininger <christophe.steininger@gmail.com>
2024-03-24 12:01:06 +0000

    Improve random game generation.

--- src/random.cpp
+++ src/random.cpp
@@ -1,3 +1,10 @@
+/*
+
+This program will generate random valid games of Connect 4.
+Used to generate games for testing or benchmarking performance.
+
+*/
+
 #include <iostream>
 #include <random>
 
@@ -7,16 +14,20 @@
 
 static inline constexpr int NUM_GAMES = 1000;
 
-// Range of moves play in each generated game. Min is inclusive, max is exclusive.
-static inline constexpr int MIN_MOVES = 29;
-static inline constexpr int MAX_MOVES = BOARD_WIDTH * BOARD_HEIGHT;
-
 // Range of moves left until the end of the game if all moves are optimal. Min is inclusive, max is exclusive.
 static inline constexpr int MIN_MOVES_LEFT = 3;
 static inline constexpr int MAX_MOVES_LEFT = 14;
 
-// Maximum number of random moves to try before giving up.
-static inline constexpr int MAX_ATTEMPTS_PER_MOVE = 10;
+// Range of moves play in each generated game. Min is inclusive, max is exclusive.
+static inline constexpr int MIN_MOVES = 29;
+static inline constexpr int MAX_MOVES = BOARD_WIDTH * BOARD_HEIGHT - MIN_MOVES_LEFT + 1;
+
+
+static_assert(MIN_MOVES_LEFT < MAX_MOVES_LEFT);
+
+static_assert(0 < MIN_MOVES);
+static_assert(MIN_MOVES < MAX_MOVES);
+static_assert(MAX_MOVES <= 1 + BOARD_WIDTH * BOARD_HEIGHT - MIN_MOVES_LEFT);
 
 
 static std::mt19937 engine{0};
@@ -35,13 +46,13 @@ static void print_game(const Position &pos, int moves[], int score) {
     for (int i = 0; i < pos.num_moves(); i++) {
         assert(0 <= moves[i] && moves[i] < BOARD_WIDTH);
 
-        std::cout << (moves[i] + 1);
+        std::cout << moves[i];
     }
 
     std::cout << " " << score << std::endl;
 }
 
-static bool is_game_close_to_end(const Position& pos) {
+static bool is_game_close_to_end(const Position &pos) {
     if (pos.num_moves() < 7) {
         return false;
     }
@@ -55,37 +66,44 @@ static bool is_game_close_to_end(const Position& pos) {
     return solver.solve(pos, win - 1, win) >= win || solver.solve(pos, loss, loss + 1) <= loss;
 }
 
-static int play_random_move(Position& pos) {
-    for (int i = 0; i < MAX_ATTEMPTS_PER_MOVE; i++) {
-        int move = dist(engine) % BOARD_WIDTH;
-        
-        if (pos.is_move_valid(move)) {
-            board before_move = pos.move(move);
-            if (is_game_close_to_end(pos)) {
-                pos.unmove(before_move);
-            } else {
-                return move;
+static int get_random_move(Position &pos) {
+    int possible_moves[BOARD_WIDTH];
+    int num_valid_moves = 0;
+    
+    for (int i = 0; i < BOARD_WIDTH; i++) {
+        if (pos.is_move_valid(i)) {
+            board before_move = pos.move(i);
+
+            if (!is_game_close_to_end(pos)) {
+                possible_moves[num_valid_moves] = i;
+                num_valid_moves++;
             }
+
+            pos.unmove(before_move);
         }
     }
 
-    return -1;
+    if (num_valid_moves == 0) {
+        return -1;
+    }
+
+    int move_index = dist(engine) % num_valid_moves;
+    return possible_moves[move_index];
 }
 
-static bool try_random_game() {
+static bool try_random_game(int num_moves) {
     Position pos{};
-    
-    int num_moves = (dist(engine) % (MAX_MOVES - MIN_MOVES)) + MIN_MOVES;
     int moves[BOARD_WIDTH * BOARD_HEIGHT];
 
     for (int i = 0; i < num_moves; i++) {
-        int move = play_random_move(pos);
+        int move = get_random_move(pos);
         
         // Cannot keep playing if we could not find a move to play.
         if (move == -1) {
             return false;
         }
 
+        pos.move(move);
         moves[i] = move;
     }
 
@@ -103,12 +121,25 @@ static bool try_random_game() {
 
 int main() {
     std::cout.imbue(std::locale(""));
-    std::cout << solver.get_settings_string();
-    std::cout << "Generating " << NUM_GAMES << " random games on " << BOARD_WIDTH << " x " << BOARD_HEIGHT << " boards." << std::endl;
-    std::cout << std::endl;
+    std::cout << solver.get_settings_string()
+              << std::endl
+              << "Generating " << NUM_GAMES << " random games:" << std::endl;
+
+    int game_lengths[BOARD_WIDTH * BOARD_HEIGHT]{};
 
     for (int i = 0; i < NUM_GAMES; i++) {
-        while (!try_random_game());
+        int num_moves = (dist(engine) % (MAX_MOVES - MIN_MOVES)) + MIN_MOVES;
+        
+        while (!try_random_game(num_moves));
+        game_lengths[num_moves]++;
+    }
+
+    std::cout << "Done." << std::endl
+              << std::endl
+              << "Generated games by length:" << std::endl;
+    
+    for (int i = MIN_MOVES; i < MAX_MOVES; i++) {
+        std::cout << i << ": " << game_lengths[i] << std::endl;
     }
 
     return 0;
